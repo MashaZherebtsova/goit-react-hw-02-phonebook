27dad698872480208205b128a9d598d3ac8af383
MashaZherebtsova <113789649+MashaZherebtsova@users.noreply.github.com>
2023-05-01 18:11:22 +0300

    update0105

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,52 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { ContactsForm } from "./ContactsForm/ContactsForm";
+
+import React, { Component } from 'react'
+import { ContactsList } from "./ContactsList/ContactsList";
+import { Filter } from "./Filter/Filter";
+import toast, { Toaster } from 'react-hot-toast';
+import css from './App.module.css'
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: ""
+  }
+handleAddContact = (contact) => {
+  if (this.state.contacts.some((item) => item.name === contact.name)){
+    toast.error("Contact already exists");
+    return true
+  }
+this.setState ((prevState)=> {
+return {
+  contacts : [...prevState.contacts, contact]
+}
+})
+return false
+}
+handleDeleteContact = (id) => {
+this.setState((prevState)=> {
+  return {
+    contacts: prevState.contacts.filter((contact => contact.id !== id))
+  }
+})
+}
+handleChangeFilter = (e) => {
+  this.setState ({filter: e.target.value})
+}
+handleFilterContacts = () => {
+  return this.state.contacts.filter(contact=> contact.name.toLowerCase().includes(this.state.filter.toLowerCase().trim()))
+}
+  render() {
+    return (
+      <>
+     <h1 className={css.title}>Phonebook</h1>
+    <ContactsForm addContact = {this.handleAddContact}/>
+    <h2 className={css.title}> Contacts</h2>
+    <Filter value = {this.state.filter} handleChange={this.handleChangeFilter}/>
+    <ContactsList contacts={this.handleFilterContacts()} deleteContact ={this.handleDeleteContact} />
+    <Toaster/>
+    </>
+    )
+  }
+}
+
